306d062e24e7ef47e2c7f2873235e2e64b620ae4
Heather Friedman <hfried25@gmail.com>
2020-09-17 12:29:02 -0400

    continue working on link page

--- src/Links.jsx
+++ src/Links.jsx
@@ -2,5 +2,36 @@
 import React from 'react';
 
 export default function Links() {
-  return <div>Useful Links</div>;
+  return (
+    <div>
+      <div>Useful Links</div>
+
+      <ul>
+        <li>
+          <a href="https://www.w3.org/WAI/standards-guidelines/wcag/">
+            https://www.w3.org/WAI/standards-guidelines/wcag/
+          </a>
+        </li>
+        <li>
+          UK Gov't Thing:
+          <a href="https://accessibility.blog.gov.uk/2017/02/24/what-we-found-when-we-tested-tools-on-the-worlds-least-accessible-webpage/">
+            https://accessibility.blog.gov.uk/2017/02/24/what-we-found-when-we-tested-tools-on-the-worlds-least-accessible-webpage/
+          </a>
+        </li>
+        <li>
+          <a href="https://idahotc.com/Portals/0/Resources/636/WCAG_2.1_Checklist.pdf">
+            https://idahotc.com/Portals/0/Resources/636/WCAG_2.1_Checklist.pdf
+          </a>
+        </li>
+
+        <li>
+          Web Accessibility in the News:
+          <a href="https://www.cnbc.com/2019/10/07/dominos-supreme-court.html">
+            https://www.cnbc.com/2019/10/07/dominos-supreme-court.html
+          </a>
+        </li>
+        <li>Link to our medium articles etc</li>
+      </ul>
+    </div>
+  );
 }
