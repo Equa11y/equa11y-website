8326163c5276fab80392d3e94efb3dc891769a2b
Heather Friedman <hfried25@gmail.com>
2020-09-16 20:15:18 -0400

    began working on navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { hot } from "react-hot-loader/root";
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+import NavBar from './NavBar';
 
 class App extends React.Component {
   render() {
     const { name } = this.props;
     return (
       <>
+        <NavBar />
         <h1>Hello {name}</h1>
       </>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,43 @@
+/* eslint-disable prettier/prettier */
 // const webpack = require("webpack");
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: ["react-hot-loader/patch", "./src/index.js"],
+  entry: ['react-hot-loader/patch', './src/index.js'],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   // mode: process.env.NODE_ENV,
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
     alias: {
-      "react-dom": "@hot-loader/react-dom",
+      'react-dom': '@hot-loader/react-dom',
     },
   },
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
   },
 };
