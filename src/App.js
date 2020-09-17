bf87aa09427d226ed4f741b7f72b3b9d52644090
Heather Friedman <hfried25@gmail.com>
2020-09-17 12:09:10 -0400

    working nav bar with react router, no styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { Route, Switch } from 'react-router-dom';
+import ExamplePage from './ExamplePage';
 import NavBar from './NavBar';
+import Main from './Main';
+import About from './About';
+import Docs from './Docs';
+import Links from './Links';
 
 class App extends React.Component {
   render() {
-    const { name } = this.props;
     return (
-      <>
-        <NavBar />
-        <h1>Hello {name}</h1>
-      </>
+      <div>
+        <div>
+          <NavBar />
+        </div>
+        <div>
+          <Switch>
+            <Route path="/example">
+              <ExamplePage />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/docs">
+              <Docs />
+            </Route>
+            <Route path="/links">
+              <Links />
+            </Route>
+
+            <Route exact path="/">
+              <Main />
+            </Route>
+          </Switch>
+        </div>
+      </div>
     );
   }
 }
---
+++ src/Docs.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+export default function Docs() {
+  return <div>Docs</div>;
+}
