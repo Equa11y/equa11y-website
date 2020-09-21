cf59a510044071854d3643959237a1844f63ff72
Heather Friedman <hfried25@gmail.com>
2020-09-21 13:39:12 -0400

    updated pie chart with smaller label

--- src/components/ThirtyPercentPie.jsx
+++ src/components/ThirtyPercentPie.jsx
@@ -3,26 +3,24 @@ import { PieChart, Pie, Cell } from 'recharts';
 
 export default function PieChartThirty() {
   const data = [
-    { name: 'Errors Caught', value: 30 },
-    { name: 'Manual Testing Required', value: 70 },
+    { name: 'Errors Caught by Automated Testing', value: 30 },
+    { name: 'Errors Requiring Manual Testing', value: 70 },
   ];
 
-  const COLORS = ['#3A6884', '#695282', '#A50060', '#BF5000'];
+  const COLORS = ['#3A6884', '#695282'];
 
   const renderCustomizedLabel = data => {
     return data.name;
   };
 
   return (
-    <PieChart width={800} height={800}>
+    <PieChart fontSize="20px" width={800} height={800}>
       <Pie
         data={data}
         cx={400}
         cy={200}
-        labelLine={false}
         label={renderCustomizedLabel}
         outerRadius={120}
-        fill="#8884d8"
         dataKey="value"
       >
         {data.map((entry, index) => (
