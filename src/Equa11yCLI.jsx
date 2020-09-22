f02bb5b1da682617b69dd8efc49279f10245bbac
Heather Friedman <hfried25@gmail.com>
2020-09-22 19:50:05 -0400

    updated alt text;

--- src/Equa11yCLI.jsx
+++ src/Equa11yCLI.jsx
@@ -38,7 +38,7 @@ export default function Equa11yCLI() {
           <div className="mainChild">
             <h2>How To Use</h2>
             <p>
-              Simply install our{' '}
+              Simply install our
               <a href="https://www.npmjs.com/package/equa11y" target="_blank" role="link">
                 npm package
               </a>{' '}
@@ -54,7 +54,12 @@ export default function Equa11yCLI() {
               deployed website. You can manually test different end points of your application by
               providing it in the url.
             </p>
-            <img src={equa11y} height="600" width="900"></img>
+            <img
+              src={equa11y}
+              height="600"
+              width="900"
+              alt="screenshot of command line tool initial page with request for input url"
+            ></img>
             <p>
               Once you load a website, you will have access to a dropdown menu in the command line.
               Here you will be able to choose to refresh equa11y to reassess the same url, try a new
@@ -62,27 +67,47 @@ export default function Equa11yCLI() {
               program broken down into categories based on severity of error. Lastly, you will see
               recommendations for manual tests to perform.
             </p>
-            <img src={equa11ypage1} height="600" width="900"></img>
+            <img
+              src={equa11ypage1}
+              height="600"
+              width="900"
+              alt="screenshot of command line tool with types of errors categorized by severity"
+            ></img>
             <p>
               You can now easily navigate using the keyboard to each issue type. Click enter to see
               more information. The information included in this sublevel includes a summary of the
               error type.
             </p>
-            <img src={errortype} height="600" width="900"></img>
+            <img
+              src={errortype}
+              height="600"
+              width="900"
+              alt="screenshot of npm pacakge with dropdown menu showing error types"
+            ></img>
             <p>
               On this third sublevel, you can see a link to more information about the error, a
               recommendation for fixing the error, along with the html of where the error is located
               in the code base. To access the url for more information, simply navigate to the line
               using your keyboard and press enter.
             </p>
-            <img src={sublevel} height="700" width="900"></img>
+            <img
+              src={sublevel}
+              height="700"
+              width="900"
+              alt="screenshot of command line tool with dropdown menu showing url for more information, recommendations for fixing error, and html of error in codebase"
+            ></img>
             <p>
               Lastly, you have access to 15 common errors that require manual testing. You can
               navigate through this menu using the keyboard and press enter for each error. You can
               then press enter to be taken to the WCAG guidelines which can provide additional
               information for manual testing of this common accessibility error.
             </p>
-            <img src={manualtesting} height="800" width="900"></img>
+            <img
+              src={manualtesting}
+              height="800"
+              width="900"
+              alt="screenshot of command line tool with dropdown menu showing manual testing recommendations and url to find more information"
+            ></img>
           </div>
         </div>
       </div>
