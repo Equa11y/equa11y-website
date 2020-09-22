b08c014cc47287ece3e91d8475bdb5a85cec027e
Heather Friedman <hfried25@gmail.com>
2020-09-22 15:12:12 -0400

    heroku server build

--- server/server.js
+++ server/server.js
@@ -2,4 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(process.env.PORT);
+const port = process.env.PORT || 3001;
+
+app.use('/', express.static(path.join(__dirname, '../dist')));
+app.get('*', (_request, response) =>
+  response.sendFile(path.resolve(`${__dirname}/../dist/index.html`)),
+);
+app.listen(port, () => console.log(`server listening on ${port}.`));
