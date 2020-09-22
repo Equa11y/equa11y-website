84b69c4cfab9b7f5d9286a1e2518a6935dda495c
Heather Friedman <hfried25@gmail.com>
2020-09-22 17:20:14 -0400

    updated nav bar still need to add equa11y cli page

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,39 +4,38 @@ import wa11y from './images/wa11y.png';
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
-  const [isClicked, setIsClicked] = useState(false);
+  // const [isClicked, setIsClicked] = useState(false);
   return (
-    <div className="navbar">
-      <Link to="/">
-        <button className="wally" role="button">
-          <img src={wa11y} alt="dog with name wally" width="100" height="100"></img>
-        </button>
-      </Link>
-
-      <div className="title">Equa11y</div>
-      <div className="dropdown">
-        <button onClick={() => setIsClicked(!isClicked)} className="dropbtn">
-          Menu
-        </button>
-        {isClicked && (
-          <div className="dropdown-content">
-            <Link to="/example">
-              <button onClick={() => setIsClicked(false)} className="content">
-                <div>Example Page</div>
-              </button>
-            </Link>
-            <Link to="links">
-              <button onClick={() => setIsClicked(false)} className="content">
-                <div>Useful Links</div>
-              </button>
-            </Link>
-            <Link to="about">
-              <button onClick={() => setIsClicked(false)} className="content">
-                <div>About</div>
-              </button>
-            </Link>
-          </div>
-        )}
+    <div>
+      <div className="navbar">
+        <Link to="/">
+          <button className="wally" role="button">
+            <img src={wa11y} alt="dog with name wally" width="100" height="100"></img>
+          </button>
+        </Link>
+        <div className="title">Equa11y</div>
+      </div>
+      <div className="dropdown navbar">
+        <Link to="/example">
+          <button className="content">
+            <div>Example Page</div>
+          </button>
+        </Link>
+        <Link to="/links">
+          <button className="content">
+            <div>Useful Links</div>
+          </button>
+        </Link>
+        <Link to="/about">
+          <button className="content">
+            <div>About Us</div>
+          </button>
+        </Link>
+        <Link to="/equa11y">
+          <button className="content">
+            <div>Equa11y CLI</div>
+          </button>
+        </Link>
       </div>
     </div>
   );
