d3c080246af219b4fa16482c3d70d30bed61e1ec
Heather Friedman <hfried25@gmail.com>
2020-09-22 18:41:08 -0400

    working on equa11ycli page

--- src/Equa11yCLI.jsx
+++ src/Equa11yCLI.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
 
 export default function Equa11yCLI() {
-  return <div>YO</div>;
+  return (
+    <div className="main">
+      <div id="introduction" className="mainChild">
+        <h1>Equa11y Command Line Tool</h1>
+      </div>
+      <div className="main-cards">
+        <div id="useSite" className="mainChild">
+          <h2>Title</h2>
+          <p></p>
+        </div>
+        <div id="accessImportance" className="mainChild">
+          <h2>Title</h2>
+          <p></p>
+        </div>
+        <div id="wa11yDog" className="mainChild">
+          <h2>Title</h2>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
 }
